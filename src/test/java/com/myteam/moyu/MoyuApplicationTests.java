16e091770ceafb5e2750d2f0b6e3228e8323c31d
SWY <swy740646093@foxmail.com>
2020-04-09 23:42:21 +0800

    SWY moyu first commit

---
+++ src/test/java/com/myteam/moyu/MoyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myteam.moyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoyuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
